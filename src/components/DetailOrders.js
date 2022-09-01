479c4f253c9b84814cc817464c991d1c43c3748a
Sharon Konki <sharonkonki@gmail.com>
2022-09-01 12:07:07 -0500

    style changes to order details page

--- src/components/DetailOrders.js
+++ src/components/DetailOrders.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import axios from 'axios';
 import homepagepic3 from "../assets/homepagepicture3.jpeg";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { useParams } from "react-router-dom";
 
 function withParams(Component) {
@@ -63,7 +65,22 @@ function withParams(Component) {
             src={homepagepic3}
             alt="Orders"
           />
-          <div class = "overlay">
+           <Card border="dark" style={{ width: '81rem' }}>
+      <Card.Header as="h5" >Order Details for {this.state.orders.customer.customerName}</Card.Header>
+      <Card.Body>
+        {/* <Card.Title>Order Details for {this.state.orders.customer.customerName}</Card.Title> */}
+        <Card.Text>
+        <p>Order ID: {this.state.orders.orderId}</p>
+        <p>Customer Name: {this.state.orders.customer.customerName}</p>
+        <p>Order Date: {this.state.orders.orderDate}</p>
+        <p>Delivery Date: {this.state.orders.deliveryDate}</p>
+        <p>Order Quantity: {this.state.orders.orderDetails[0].qty}</p>
+        <p>Seed Name: {this.state.orders.orderDetails[0].seed.seedName}</p>
+        </Card.Text>
+        <Button variant="dark"  onClick={()=>window.open("/orders", '_self')}>Back to Orders</Button>
+      </Card.Body>
+    </Card>
+          {/* <div class = "overlay">
             <h2 class = "header" className='text-center'> Order Details</h2>
             </div>
           </div>
@@ -73,7 +90,7 @@ function withParams(Component) {
             <p>Order Date: {this.state.orders.orderDate}</p>
             <p>Delivery Date: {this.state.orders.deliveryDate}</p>
             <p>Order Quantity: {this.state.orders.orderDetails[0].qty}</p>
-            <p>Seed Name: {this.state.orders.orderDetails[0].seed.seedName}</p>
+            <p>Seed Name: {this.state.orders.orderDetails[0].seed.seedName}</p> */}
         </div>
                </div>
                   )
